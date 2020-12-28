736108964a5ad88b628ff97169771e70ca237bed
Lohi <lohi3152@gmail.com>
2020-12-28 16:33:35 +0800

    define CeremonyFormModel

--- src/views/About.vue
+++ src/views/About.vue
@@ -15,51 +15,27 @@ import Longevity from '@/components/Longevity.vue';
 import Ceremony from '@/components/Ceremony.vue';
 import PrayFor from '@/components/PrayFor.vue';
 
+import {
+  AttendInfo, CeremonyForm, LongevityBoard, PersonalInformation, PrayBoards,
+} from '@/models/CeremonyFormModel';
+
 export default Vue.extend({
   data() {
     return {
-      personalInformation: {
-        uid: 'afas001451',
-        name: '',
-        address: '',
-        phone: '',
-        telephone: '',
-        email: '',
-        line: '',
-        wechat: '',
-      },
+      personalInformation: [
+        new PersonalInformation('', '', '', '', '', '', ''),
+      ],
       longevity: [
-        {
-          id: '',
-          name: '',
-        },
+        new LongevityBoard('peoplename'),
       ],
       prayFor: [
-        {
-          name: '',
-          targetSelection: [
-            '冤親債主',
-            '歷代祖先',
-            '累世父母師長',
-            '未出生子女',
-            '誤殺誤傷之眾生',
-            '境內一切眾生(地基主)(請填寫住家地址)',
-          ],
-          targetValue: '45678979',
-          targetName: '',
-          targetAddress: '',
-        },
+        new PrayBoards('', '', ''),
       ],
       ceremony: [
-        {
-          name: '',
-          gender: '',
-          age: '',
-          comeDate: '',
-          backDate: '',
-          note: '',
-        },
+        new AttendInfo('', '', '', '', '', ''),
       ],
+
+      form: new CeremonyForm(),
     };
   },
   components: {
