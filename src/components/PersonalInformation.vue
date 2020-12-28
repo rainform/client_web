b240862ecc7ed99876a01c5a506582d2470f57e1
Lohi <lohi3152@gmail.com>
2020-12-28 16:32:59 +0800

    Setup basic pages and forms

---
+++ src/components/PersonalInformation.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="personalInformation">
+    <h2>個人基本資料</h2>
+    <form>
+      <div>
+        <label>uid</label>
+        <input type="text" v-model="personal.uid" disabled>
+      </div>
+      <div class="input-group">
+        <label for="personal_name">name</label>
+        <input id="personal_name" type="text">
+      </div>
+      <div class="input-group">
+        <label for="personal_address">address</label>
+        <input id="personal_address" type="text">
+      </div>
+      <div class="input-group">
+        <label for="personal_phone">phone</label>
+        <input id="personal_phone" type="text">
+      </div>
+      <div class="input-group">
+        <label for="personal_telephone">telephone</label>
+        <input id="personal_telephone" type="text">
+      </div>
+      <div class="input-group">
+        <label for="personal_email">email</label>
+        <input id="personal_email" type="text">
+      </div>
+      <div class="input-group">
+        <label for="personal_line">line</label>
+        <input id="personal_line" type="text">
+      </div>
+      <div class="input-group">
+        <label for="personal_wechat">wechat</label>
+        <input id="personal_wechat" type="text">
+      </div>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  props: {
+    personal: {
+      type: Object,
+      default: () => ({
+        uid: 'afas001451',
+        name: '',
+        address: '',
+        phone: '',
+        telephone: '',
+        email: '',
+        line: '',
+        wechat: '',
+      }),
+    },
+  },
+  data() {
+    return {
+    };
+  },
+});
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,72 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about" style="text-align: left;">
+    <h1>This is a form.</h1>
+    <Personal :personal="personalInformation"/>
+    <Longevity :longevity="longevity"/>
+    <PrayFor :prayFor="prayFor"/>
+    <Ceremony :ceremony="ceremony"/>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Personal from '@/components/PersonalInformation.vue';
+import Longevity from '@/components/Longevity.vue';
+import Ceremony from '@/components/Ceremony.vue';
+import PrayFor from '@/components/PrayFor.vue';
+
+export default Vue.extend({
+  data() {
+    return {
+      personalInformation: {
+        uid: 'afas001451',
+        name: '',
+        address: '',
+        phone: '',
+        telephone: '',
+        email: '',
+        line: '',
+        wechat: '',
+      },
+      longevity: [
+        {
+          id: '',
+          name: '',
+        },
+      ],
+      prayFor: [
+        {
+          name: '',
+          targetSelection: [
+            '冤親債主',
+            '歷代祖先',
+            '累世父母師長',
+            '未出生子女',
+            '誤殺誤傷之眾生',
+            '境內一切眾生(地基主)(請填寫住家地址)',
+          ],
+          targetValue: '45678979',
+          targetName: '',
+          targetAddress: '',
+        },
+      ],
+      ceremony: [
+        {
+          name: '',
+          gender: '',
+          age: '',
+          comeDate: '',
+          backDate: '',
+          note: '',
+        },
+      ],
+    };
+  },
+  components: {
+    Personal,
+    Longevity,
+    PrayFor,
+    Ceremony,
+  },
+});
+</script>
